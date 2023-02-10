e6c4a181286c7bb02be0bd11f8cf052ba8f3043a
Coderzzz1 <abdullah_abaga1203@yahoo.com>
2023-02-11 00:04:04 +0800

    sample

--- intayon/lib/main.dart
+++ intayon/lib/main.dart
@@ -28,14 +28,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-
       ),
     );
   }
